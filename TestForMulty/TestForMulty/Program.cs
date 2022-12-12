9edbf0170c34078f6f34b1be4b6803de72a21be3
NesetBurakAtay <109911079+nesetburakatay@users.noreply.github.com>
2022-12-12 21:53:56 +0300

    fdsa

--- TestForMulty/TestForMulty/Program.cs
+++ TestForMulty/TestForMulty/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!w");
 Console.WriteLine("Hello, World!2");
 sdsdqwe
